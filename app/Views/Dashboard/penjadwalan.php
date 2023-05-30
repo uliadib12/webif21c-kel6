1918e86cce6de7acffc24c81d9afab5b8080cf18
uliadib12 <uliadib@gmail.com>
2023-05-30 15:34:02 +0700

    edit

--- app/Views/Dashboard/penjadwalan.php
+++ app/Views/Dashboard/penjadwalan.php
@@ -8,7 +8,7 @@
         <div class="table-title">
             <div class="row">
                 <div class="col-sm-6">
-                    <h2>Manage <b>Event's</b></h2>
+                    <h2>Tabel <b>Kegiatan</b></h2>
                 </div>
                 <div class="col-sm-6">
                     <a href="#addEmployeeModal" class="btn btn-add" data-toggle="modal"><i
